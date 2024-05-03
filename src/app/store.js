d4cbc34097f33c48be8fc5be7440651bb9dacc56
JesusMartinezClavel <jmcvalles@gmail.com>
2024-05-03 18:18:08 +0200

    FEAT -> Created store.js for redux

---
+++ src/app/store.js
@@ -0,0 +1,39 @@
+// Redux modules
+import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import { encryptTransform } from "redux-persist-transform-encrypt";
+import { thunk } from "redux-thunk";
+import storage from "redux-persist/lib/storage";
+
+// Redux Slices
+
+
+const reducers = combineReducers(
+    {
+
+    }
+)
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    transforms: [
+        encryptTransform({
+            secretKey: 'arenita',
+            onerror: function (error) {
+                console.log(error.message);
+            }
+        })
+    ]
+}
+
+const persistedReducer = persistReducer(persistConfig, reducers)
+
+export default configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: false
+        }).concat(thunk)
+})
