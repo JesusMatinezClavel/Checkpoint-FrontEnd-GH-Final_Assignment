2c7a62ec569a1d1f7b1cdafa0cee5c0332c1eb9a
JesusMartinezClavel <jmcvalles@gmail.com>
2024-05-03 19:38:09 +0200

    FEAT -> Created inputHandler for login and register

--- src/common/header/header.jsx
+++ src/common/header/header.jsx
@@ -19,6 +19,43 @@ export const Header = () => {
     const [showLogin, setShowLogin] = useState(false)
     const [showRegister, setShowRegister] = useState(false)
 
+    const [registerData, setRegisterData] = useState({
+        avatar: "",
+        name: "",
+        email: "",
+        password: ""
+    })
+
+    const [loginData, setLoginData] = useState({
+        email: "",
+        password: ""
+    })
+
+    console.log(loginData);
+    console.log(registerData);
+
+
+    const inputHandler = (e) => {
+        showLogin
+            ? (
+                setLoginData((prevState) => ({
+                    ...prevState,
+                    [e.target.name]: e.target.value
+                }))
+            )
+            : null
+
+        showRegister
+            ? (
+                setRegisterData((prevState) => ({
+                    ...prevState,
+                    [e.target.name]: e.target.value
+                }))
+            )
+            : null
+
+    }
+
     const toggleLogin = () => {
         setShowRegister(false)
         showLogin
@@ -43,10 +80,16 @@ export const Header = () => {
             <CCard className={showLogin ? "card-login" : 'hidden'}>
                 <CInput
                     name={'email'}
+                    type={'text'}
+                    value={loginData.email || ""}
+                    onChange={(e) => inputHandler(e)}
                     placeholder={'input email'}
                 />
                 <CInput
                     name={'password'}
+                    type={'text'}
+                    value={loginData.password || ""}
+                    onChange={(e) => inputHandler(e)}
                     placeholder={'input password'}
                 />
                 <CButton className={'button-loggin'} title={'login'} />
@@ -54,36 +97,39 @@ export const Header = () => {
 
             {/* Register Card */}
             <CCard className={showRegister ? "card-register" : 'hidden'}>
-                <div className="register-avatar">
+                <div className="register-inputs">
                     <CInput
                         name={'avatar'}
                         type={'file'}
+                        value={registerData.avatar || ""}
                         placeholder={'input avatar'}
+                        onChange={(e) => inputHandler(e)}
                     />
+                    <div className="register-info">
+                        <CInput
+                            name={'name'}
+                            type={'text'}
+                            value={registerData.name || ""}
+                            placeholder={'input name'}
+                            onChange={(e) => inputHandler(e)}
+                        />
+                        <CInput
+                            name={'email'}
+                            type={'text'}
+                            value={registerData.email || ""}
+                            placeholder={'input email'}
+                            onChange={(e) => inputHandler(e)}
+                        />
+                        <CInput
+                            name={'password'}
+                            type={'text'}
+                            value={registerData.password || ""}
+                            placeholder={'input password'}
+                            onChange={(e) => inputHandler(e)}
+                        />
+                    </div>
                 </div>
-                <div className="register-info">
-                    <CInput
-                        name={'name'}
-                        type={'text'}
-                        placeholder={'input name'}
-                    />
-                    <CInput
-                        name={'birthdate'}
-                        type={'date'}
-                        placeholder={'input birthdate'}
-                    />
-                    <CInput
-                        name={'email'}
-                        type={'text'}
-                        placeholder={'input email'}
-                    />
-                    <CInput
-                        name={'password'}
-                        type={'text'}
-                        placeholder={'input password'}
-                    />
-                <CButton className={'button-loggin'} title={'login'} />
-                </div>
+                <CButton className={'button-loggin'} title={'register'} />
             </CCard>
         </div>
     )
