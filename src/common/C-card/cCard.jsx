8b50ef5778d4188e14a91993bcce1b189679e7ff
JesusMartinezClavel <jmcvalles@gmail.com>
2024-05-03 17:35:45 +0200

    FEAT -> Created custom card

---
+++ src/common/C-card/cCard.jsx
@@ -0,0 +1,7 @@
+import './cCard.css'
+
+export const CCard = ({ children }) => {
+    return (
+        <div className="card-design">{children}</div>
+    )
+}
--- src/views/welcome/welcome.jsx
+++ src/views/welcome/welcome.jsx
@@ -1,12 +1,15 @@
 import './welcome.css'
 
 import { CButton } from "../../common/C-button/cButton";
+import { CCard } from "../../common/C-card/cCard";
 
 
 export const Welcome = () => {
     return (
         <div className="welcome-design">
-            <CButton title={'button'}/>
+            <CCard>
+                <CButton title={'button'} />
+            </CCard>
         </div>
     )
 }
