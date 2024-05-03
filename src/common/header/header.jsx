c3faf91ada5ad525d9f98783eaf2050b0e82f388
JesusMartinezClavel <jmcvalles@gmail.com>
2024-05-03 20:27:11 +0200

    FEAT -> Created inputHandler and checkError functions || Linked errors's hooks with errorMsg to change it when there's an error an disable the rest of the inputs

--- src/common/header/header.jsx
+++ src/common/header/header.jsx
@@ -3,37 +3,54 @@ import './header.css'
 
 // Methods/Modules
 import { useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { validate } from "../../utils/validator";
 
 // Custom Elements
 import { Navigator } from "../navigator/navigator";
 import { CCard } from "../C-card/cCard";
 import { CInput } from "../C-input/cInput";
 import { CButton } from "../C-button/cButton";
+import { CText } from '../C-text/cText';
 
 
 
 
 export const Header = () => {
 
+    // INSTANCES
     const [showLogin, setShowLogin] = useState(false)
     const [showRegister, setShowRegister] = useState(false)
 
+    // HOOKS
     const [registerData, setRegisterData] = useState({
         avatar: "",
         name: "",
         email: "",
         password: ""
     })
+    const [registerDataError, setRegisterDataError] = useState({
+        avatarError: "",
+        nameError: "",
+        emailError: "",
+        passwordError: ""
+    })
 
     const [loginData, setLoginData] = useState({
         email: "",
         password: ""
     })
+    const [loginDataError, setLoginDataError] = useState({
+        emailError: "",
+        passwordError: ""
+    })
 
-    console.log(loginData);
-    console.log(registerData);
+    const [errorMsg, setErrorMsg] = useState("")
 
+    // LOGIC
+    useEffect(() => {
+        document.title = "Welcome";
+    }, [])
 
     const inputHandler = (e) => {
         showLogin
@@ -54,7 +71,61 @@ export const Header = () => {
             )
             : null
 
+        if (e.target.value === "") {
+            setErrorMsg("")
+        }
+
     }
+
+    const checkError = (e) => {
+
+        const valid = validate(e.target.name, e.target.value)
+
+        showLogin
+            ? (
+                setLoginDataError((prevState) => ({
+                    ...prevState,
+                    [e.target.name + 'Error']: valid
+                }))
+            )
+            : null
+
+        showRegister
+            ? (
+                setRegisterDataError((prevState) => ({
+                    ...prevState,
+                    [e.target.name + 'Error']: valid
+                }))
+            )
+            : null
+
+    }
+
+    console.log(loginDataError);
+    console.log(errorMsg);
+
+    useEffect(() => {
+        let allErrorsCleared
+        if (showLogin) {
+            for (let element in loginDataError) {
+                if (loginDataError[element] !== "") {
+                    setErrorMsg(loginDataError[element])
+                    break
+                }
+            }
+            allErrorsCleared = Object.values(loginDataError).every(value => value === "")
+        } else if (showRegister) {
+            for (let element in registerDataError) {
+                if (registerDataError[element] !== "") {
+                    setErrorMsg(registerDataError[element])
+                    break
+                }
+                allErrorsCleared = Object.values(registerDataError).every(value => value === "")
+            }
+        }
+
+        allErrorsCleared ? setErrorMsg("") : null
+    }, [loginDataError, registerDataError])
 
     const toggleLogin = () => {
         setShowRegister(false)
@@ -79,57 +150,71 @@ export const Header = () => {
             {/* Loggin Card */}
             <CCard className={showLogin ? "card-login" : 'hidden'}>
                 <CInput
+                    disabled={errorMsg === "" ? false : errorMsg === loginDataError.emailError ? false : true}
                     name={'email'}
                     type={'text'}
                     value={loginData.email || ""}
-                    onChange={(e) => inputHandler(e)}
                     placeholder={'input email'}
+                    onChange={(e) => inputHandler(e)}
+                    onBlur={(e) => checkError(e)}
                 />
                 <CInput
+                    disabled={errorMsg === "" ? false : errorMsg === loginDataError.passwordError ? false : true}
                     name={'password'}
                     type={'text'}
                     value={loginData.password || ""}
-                    onChange={(e) => inputHandler(e)}
                     placeholder={'input password'}
+                    onChange={(e) => inputHandler(e)}
+                    onBlur={(e) => checkError(e)}
                 />
-                <CButton className={'button-loggin'} title={'login'} />
+                <CButton className={errorMsg === "" ? 'button-loggin' : 'loggin-disabled'} title={'login'} />
+                <CText title={errorMsg} />
             </CCard>
 
             {/* Register Card */}
             <CCard className={showRegister ? "card-register" : 'hidden'}>
                 <div className="register-inputs">
                     <CInput
+                        disabled={errorMsg === "" ? false : errorMsg === registerDataError.avatarError ? false : true}
                         name={'avatar'}
                         type={'file'}
                         value={registerData.avatar || ""}
                         placeholder={'input avatar'}
                         onChange={(e) => inputHandler(e)}
+                        onBlur={(e) => checkError(e)}
                     />
                     <div className="register-info">
                         <CInput
+                            disabled={errorMsg === "" ? false : errorMsg === registerDataError.nameError ? false : true}
                             name={'name'}
                             type={'text'}
                             value={registerData.name || ""}
                             placeholder={'input name'}
                             onChange={(e) => inputHandler(e)}
+                            onBlur={(e) => checkError(e)}
                         />
                         <CInput
+                            disabled={errorMsg === "" ? false : errorMsg === registerDataError.emailError ? false : true}
                             name={'email'}
                             type={'text'}
                             value={registerData.email || ""}
                             placeholder={'input email'}
                             onChange={(e) => inputHandler(e)}
+                            onBlur={(e) => checkError(e)}
                         />
                         <CInput
+                            disabled={errorMsg === "" ? false : errorMsg === registerDataError.passwordError ? false : true}
                             name={'password'}
                             type={'text'}
                             value={registerData.password || ""}
                             placeholder={'input password'}
                             onChange={(e) => inputHandler(e)}
+                            onBlur={(e) => checkError(e)}
                         />
                     </div>
                 </div>
-                <CButton className={'button-loggin'} title={'register'} />
+                <CButton className={errorMsg === "" ? 'button-register' : 'register-disabled'} title={'register'} />
+                <CText title={errorMsg} />
             </CCard>
         </div>
     )
---
+++ src/utils/validator.js
@@ -0,0 +1,91 @@
+export const validate = (type, value) => {
+    switch (type) {
+        case "title":
+        case "description":
+        case "name":
+        case "firstName":
+        case "lastName":
+        case "nickName":
+        case "nombre":
+        case "surname":
+        case "cognom":
+        case "nameError":
+        case "firstNameError":
+        case "lastNameError":
+        case "nickNameError":
+        case "nombreError":
+        case "surnameError":
+        case "cognomError":
+            if (value === "") {
+                return ""
+            }
+            if (value.length < 2) {
+                return "Name has to be at least 2 characters long";
+            }
+
+            return "";
+
+        case "email":
+        case "e-mail":
+        case "correo":
+        case "mail":
+        case "emailError":
+        case "e-mailError":
+        case "correoError":
+        case "mailError":
+            if (value === "") {
+                return ""
+            }
+            const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+            if (!emailRegex.test(value)) {
+                return "Email format must be correct!";
+            }
+
+            return "";
+
+        case "password":
+        case "passwordCheck":
+        case "passwordBody":
+        case "verifyPassword":
+        case "passwordHash":
+        case "contraseña":
+        case "passwordError":
+        case "passwordCheckError":
+        case "passwordBodyError":
+        case "verifyPasswordError":
+        case "passwordHashError":
+        case "contraseñaError":
+            if (value === "") {
+                return ""
+            }
+            const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,10}$/;
+            if (value.length < 6 || value.length > 10) {
+                return "Password has to be between 6 and 10 characters long";
+            }
+            if (!passwordRegex.test(value)) {
+                return "Password has to have an Uppercase, a lowercase and a number"
+            }
+
+            return "";
+
+        case "birthDate":
+        case "birthdate":
+
+            if (userAge(new Date(value)) < 18) {
+                return "You have to be +18 to register"
+            }
+
+            return ""
+        case "media":
+        case "profileImg":
+
+            if (typeof (value) !== 'string' || !value.match(/\.(jpeg|jpg|gif|png)$/i)) {
+                return `The image has no valid format!`
+            }
+
+            return ""
+        default:
+    }
+
+    return ""
+};
